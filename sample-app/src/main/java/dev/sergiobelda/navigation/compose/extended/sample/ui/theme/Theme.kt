f16a37bdc9a23b6cee6f6b1bb0178e95caa64006
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2024-03-18 20:53:37 +0100

    Update sample app

--- sample-app/src/main/java/dev/sergiobelda/navigation/compose/extended/sample/ui/theme/Theme.kt
+++ sample-app/src/main/java/dev/sergiobelda/navigation/compose/extended/sample/ui/theme/Theme.kt
@@ -69,14 +69,6 @@ fun SampleTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
