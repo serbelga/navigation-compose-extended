63f2d1270c1a236e3521ae38a4a1939d05bb1ffe
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2025-01-03 15:04:14 +0100

    Migrate dokka to v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,15 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.vanniktechMavenPublish) apply false
-    alias(libs.plugins.dokka)
+}
+
+dependencies {
+    dokka(projects.navigationComposeExtended)
+    dokka(projects.navigationComposeExtendedAnnotation)
+    dokka(projects.navigationComposeExtendedCompiler)
+    dokka(projects.navigationComposeExtendedWear)
 }
